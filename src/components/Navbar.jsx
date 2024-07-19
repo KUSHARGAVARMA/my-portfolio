89e86ab56efe755bc44cb702b2f61930728443f9
KUSHARGAVARMA <kushavrm008@gmail.com>
2024-07-19 16:23:53 +0530

    Theme icon changes,scrolling animation ,mobile navbar view changes ,fixed some bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { FaGithub, FaLinkedin, FaMedium, FaYoutube } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaMedium, FaYoutube, FaMoon, FaSun } from 'react-icons/fa';
 import { useTheme } from '../ThemeContext';
 import avatarImage from '../assets/HireMe.png';
 
@@ -27,20 +27,20 @@ const Navbar = () => {
     setMenuOpen(!menuOpen);
   };
 
-  const scrollToHeader = () => {
-    document.getElementById('header').scrollIntoView({ behavior: 'smooth' });
+  const scrollToComponent = (component) => {
+    document.getElementById(component).scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
     <nav className={`fixed w-full z-50 transition-all duration-300 ${scrolled ? (darkMode ? 'bg-gray-900 shadow-lg' : 'bg-white shadow-lg') : 'bg-transparent'}`}>
       <div className="container mx-auto flex justify-between items-center py-4 px-4 md:px-0">
-        <div className="flex items-center space-x-4 cursor-pointer" onClick={scrollToHeader}>
+        <div className="flex items-center space-x-4 cursor-pointer" onClick={()=>scrollToComponent("header")}>
           <div className="w-12 h-12 rounded-full overflow-hidden border-2 border-indigo-500">
             <img src={avatarImage} alt="Hire Me" className="w-full h-full object-cover" />
           </div>
           {scrolled && <h1 className={`text-2xl font-bold ${darkMode ? 'text-white' : 'text-gray-900'}`}>Kushagra Varma</h1>}
         </div>
-        <div className="md:hidden" onClick={toggleMenu}>
+        <div className="md:hidden " onClick={toggleMenu}>
           <div className="cursor-pointer">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
@@ -48,11 +48,12 @@ const Navbar = () => {
           </div>
         </div>
         <ul className={`hidden md:flex space-x-6 ${darkMode ? 'text-gray-300' : 'text-gray-700'}`}>
-          <li><a href="#header" className="hover:text-indigo-500">About</a></li>
-          <li><a href="#skills" className="hover:text-indigo-500">Skills</a></li>
-          <li><a href="#projects" className="hover:text-indigo-500">Projects</a></li>
-          <li><a href="#experience" className="hover:text-indigo-500">Experience</a></li>
-          <li><a href="#contact" className="hover:text-indigo-500">Contact</a></li>
+        <li><button onClick={() => scrollToComponent('header')} className="hover:text-indigo-500">About</button></li>
+        <li><a href="#skills" className="hover:text-indigo-500">Skills</a></li>
+        <li><button onClick={() => scrollToComponent('skills')} className="hover:text-indigo-500">Skills</button></li>
+          <li><button onClick={() => scrollToComponent('projects')} className="hover:text-indigo-500">Projects</button></li>
+          <li><button onClick={() => scrollToComponent('experience')} className="hover:text-indigo-500">Experience</button></li>
+          <li><button onClick={() => scrollToComponent('hire-me')} className="hover:text-indigo-500">Contact</button></li>
         </ul>
         <div className="hidden md:flex space-x-4 items-center">
           <a href="https://github.com/KUSHARGAVARMA" target="_blank" rel="noopener noreferrer" className="hover:text-indigo-500"><FaGithub size={24} className="text-indigo-600" /></a>
@@ -63,17 +64,20 @@ const Navbar = () => {
             onClick={toggleTheme}
             className={`py-2 px-4 rounded ${darkMode ? 'bg-gray-800 text-white' : 'bg-indigo-600 text-white'} hover:bg-indigo-700`}
           >
-            {darkMode ? 'Light Mode' : 'Dark Mode'}
+          {darkMode ? <FaSun size={20}  /> : <FaMoon size={20}  />}
+
           </button>
         </div>
       </div>
       {menuOpen && (
-        <div className={`md:hidden ${darkMode ? 'bg-gray-900' : 'bg-white'} shadow-lg`}>
+        <div className={`md:hidden absolute px-3 top-40 rounded opacity-90  transition-all duration-100 ${darkMode ? 'bg-gray-900' : 'bg-white'} shadow-lg`}>
           <ul className="flex flex-col items-center space-y-6 py-4">
-            <li><a href="#skills" className="hover:text-indigo-500" onClick={() => setMenuOpen(false)}>Skills</a></li>
-            <li><a href="#projects" className="hover:text-indigo-500" onClick={() => setMenuOpen(false)}>Projects</a></li>
-            <li><a href="#experience" className="hover:text-indigo-500" onClick={() => setMenuOpen(false)}>Experience</a></li>
-          </ul>
+            <li><button onClick={() => { scrollToComponent('header'); setMenuOpen(false); }} className="hover:text-indigo-500">About</button></li>
+
+            <li><button onClick={() => { scrollToComponent('skills'); setMenuOpen(false); }} className="hover:text-indigo-500">Skills</button></li>
+            <li><button onClick={() => { scrollToComponent('projects'); setMenuOpen(false); }} className="hover:text-indigo-500">Projects</button></li>
+            <li><button onClick={() => { scrollToComponent('experience'); setMenuOpen(false); }} className="hover:text-indigo-500">Experience</button></li>
+         </ul>
           <div className="flex flex-col items-center space-y-4 py-4">
             <a href="https://github.com/KUSHARGAVARMA" target="_blank" rel="noopener noreferrer" className="hover:text-indigo-500"><FaGithub size={24} className="text-indigo-600" /></a>
             <a href="https://www.linkedin.com/in/kushagra-varma-97433418b/" target="_blank" rel="noopener noreferrer" className="hover:text-indigo-500"><FaLinkedin size={24} className="text-indigo-600" /></a>
@@ -83,8 +87,8 @@ const Navbar = () => {
               onClick={toggleTheme}
               className={`py-2 px-4 rounded ${darkMode ? 'bg-gray-800 text-white' : 'bg-indigo-600 text-white'} hover:bg-indigo-700`}
             >
-              {darkMode ? 'Light Mode' : 'Dark Mode'}
-            </button>
+          {darkMode ? <FaSun size={20}  /> : <FaMoon size={20}  />}
+          </button>
           </div>
         </div>
       )}
